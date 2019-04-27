b52ee519fa443357cfebbcfa323c13e08e160793
HaQadosch Berraka <haqadosch@pm.me>
2019-04-27 14:51:11 +0100

    Starter Files

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -1,12 +1,3 @@
 const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
-
-const originalGetWinner = utils.getWinner
-
-utils.getWinner = (pA, pB) => pA
-
-const winner = thumbWar('Luigi', 'Mario')
-assert.equal(winner, 'Luigi')
-
-utils.getWinner = originalGetWinner
