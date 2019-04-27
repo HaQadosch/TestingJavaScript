dcec32271c2327af20ddc70c8164d3a5930bc96e
HaQadosch Berraka <haqadosch@pm.me>
2019-04-27 20:27:20 +0100

    Mock a JavaScript module in a test: Done.

--- src/no-framework/inline-module-mock.js
+++ src/no-framework/inline-module-mock.js
@@ -1,7 +1,3 @@
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
-
 function foo(implementation = () => { }) {
   const mockFoo = (...args) => {
     mockFoo.mock.calls.push(args)
@@ -17,17 +13,22 @@ function foo(implementation = () => { }) {
   return mockFoo
 }
 
-function sypOn(obj, prop) {
-  const originalValue = obj[prop]
-  obj[prop] = foo()
-  obj[prop].mockRestore = () => (obj[prop] = originalValue)
+const utilsPath = require.resolve('../utils')
+require.cache[utilsPath] = {
+  id: utilsPath,
+  filename: utilsPath,
+  loaded: true,
+  exports: {
+    getWinner: foo((pA, pB) => pA)
+  }
 }
 
-sypOn(utils, 'getWinner')
-utils.getWinner.mockImplementation((pA, pB) => pA)
+const assert = require('assert')
+const thumbWar = require('../thumb-war')
+const utils = require('../utils')
 
 const winner = thumbWar('Luigi', 'Mario')
 assert.equal(winner, 'Luigi')
 assert.deepStrictEqual(utils.getWinner.mock.calls, [['Luigi', 'Mario'], ['Luigi', 'Mario']])
 
-utils.getWinner.mockRestore()
+delete require.cache[utilsPath]
