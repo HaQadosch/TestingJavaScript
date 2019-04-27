65b45f57354dd0eedddcab420c1d10a9b1e077a4
HaQadosch Berraka <haqadosch@pm.me>
2019-04-27 14:31:47 +0100

    Trying test with riteway, but jest-runner doesn't like it.

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -1,6 +1,16 @@
-const assert = require('assert')
+// const assert = require('assert')
+const { describe } = require('riteway')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
 const winner = thumbWar('Luigi', 'Mario')
-assert.equal(winner, 'Mario')
+// assert.equal(winner, 'Mario')
+
+describe('thumbWar()', async assert => {
+  assert({
+    given: 'Luigi vs Mario',
+    should: 'Mario win',
+    actual: winner,
+    expected: 'Mario'
+  })
+})
