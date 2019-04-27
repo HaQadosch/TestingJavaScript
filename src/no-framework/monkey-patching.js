d5d9989268f0b1f4b6155b4ecf1957749a3a240e
HaQadosch Berraka <haqadosch@pm.me>
2019-04-27 14:23:02 +0100

    first assert

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -2,3 +2,5 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+const winner = thumbWar('Luigi', 'Mario')
+assert.equal(winner, 'Mario')
