71406a0785e04a728ef3ddd38d04570edde4e4e2
HaQadosch Berraka <haqadosch@pm.me>
2019-05-05 17:26:25 +0100

    Jack-jack can fly!

--- src/other/tests/super-hero.test.js
+++ src/other/tests/super-hero.test.js
@@ -2,10 +2,10 @@ import { getFlyingSuperHeros } from '../super-hero'
 
 test('should return superheros who can fly.', () => {
   const flyingHeros = getFlyingSuperHeros()
-  console.log({ flyingHeros })
 
   expect(flyingHeros).toEqual([
     { 'name': 'Dynaguy', 'powers': ['disintegration ray', 'fly'] },
-    { 'name': 'Apogee', 'powers': ['gravity control', 'fly'] }
+    { 'name': 'Apogee', 'powers': ['gravity control', 'fly'] },
+    { 'name': 'Jack-Jack', 'powers': ['shapeshifting', 'fly'] }
   ])
 })
