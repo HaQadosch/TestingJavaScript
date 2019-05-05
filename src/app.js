c462606ec31be5e409a7554a423dd3eacb098c63
HaQadosch Berraka <haqadosch@pm.me>
2019-05-05 16:18:13 +0100

    Re-init with branch config-jest-00

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import Calculator from './calculator'
 
-function App () {
+function App() {
   return (
     <React.Fragment>
       <Calculator />
-      <div style={{ marginTop: 30, textAlign: 'center' }}>
+      <div style={{marginTop: 30, textAlign: 'center'}}>
         Calculator component{' '}
-        <a href='https://codepen.io/mjijackson/pen/xOzyGX'>created</a>
+        <a href="https://codepen.io/mjijackson/pen/xOzyGX">created</a>
         {' by '}
         <br />
-        <a href='https://twitter.com/mjackson'>Michael Jackson</a> of{' '}
-        <a href='https://reacttraining.com/'>React Training</a>
+        <a href="https://twitter.com/mjackson">Michael Jackson</a> of{' '}
+        <a href="https://reacttraining.com/">React Training</a>
       </div>
     </React.Fragment>
   )
