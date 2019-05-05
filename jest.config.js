f08e7820e685045fd865368a7756e69fc9d5ec66
HaQadosch Berraka <haqadosch@pm.me>
2019-05-05 17:07:03 +0100

    Lesson 5: Support using webpack CSS modules with Jest.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom', // 'jest-environment-node'
   moduleNameMapper: {
+    '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js')
   }
 }
--- src/shared/__tests__/auto-scaling-text.js
+++ src/shared/__tests__/auto-scaling-text.js
@@ -4,5 +4,6 @@ import { render } from 'react-testing-library'
 import AutoScalingText from '../auto-scaling-text'
 
 test('renders', () => {
-  render(<AutoScalingText />)
+  const { container: { innerHTML } } = render(<AutoScalingText />)
+  console.log({ innerHTML })
 })
