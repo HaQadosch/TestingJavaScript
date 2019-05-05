f02e063a40090857d67f700ee4ffa5639099522b
HaQadosch Berraka <haqadosch@pm.me>
2019-05-05 16:59:57 +0100

    Lesson 4: Support importing CSS files with Jest’s moduleNameMapper.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom' // 'jest-environment-node'
+  testEnvironment: 'jest-environment-jsdom', // 'jest-environment-node'
+  moduleNameMapper: {
+    '\\.css$': require.resolve('./test/style-mock.js')
+  }
 }
