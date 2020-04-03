b78583548d12c9d9e8ae15a3ed7c5cce6d50e1cb
Xavier Brinon <xavier.brinon@ecs.co.uk>
2020-04-03 13:01:30 +0100

    Test React Component Event Handlers with fireEvent from React Testing Library

--- pony/src/__tests__/favorite-number.test.tsx
+++ pony/src/__tests__/favorite-number.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FavoriteNumber } from '../favorite-number';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'
 
 describe('<FavoriteNumber />', () => {
@@ -8,5 +8,13 @@ describe('<FavoriteNumber />', () => {
     const { getByLabelText } = render(<FavoriteNumber />)
     const input = getByLabelText(/Favorite Number/i)
     expect(input).toHaveAttribute('type', 'number')
+  })
+
+  test('entering an invalide value triggers an error message', () => {
+    const { getByLabelText, getByRole } = render(<FavoriteNumber />)
+    const input = getByLabelText(/Favorite Number/i)
+    fireEvent.change(input, { target: { value: 20 } })
+    const alert = getByRole('alert')
+    expect(alert).toHaveTextContent(/the number is invalid/i)
   })
 })
