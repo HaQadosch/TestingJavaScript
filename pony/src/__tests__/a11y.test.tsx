a5a221d0db233de0d568947ab5c395ff4fb21de2
Xavier Brinon <xavier.brinon@ecs.co.uk>
2020-04-03 14:49:15 +0100

    Test Accessibility of Rendered React Components with jest-axe

---
+++ pony/src/__tests__/a11y.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { axe, toHaveNoViolations } from 'jest-axe'
+
+expect.extend(toHaveNoViolations)
+
+function InaccessibleForm() {
+  return (
+    <form>
+      <input placeholder="email" />
+    </form>
+  )
+}
+
+function AccessibleForm() {
+  return (
+    <form>
+      <label htmlFor="username">Username</label>
+      <input id="username" placeholder="username" />
+    </form>
+  )
+}
+
+test('inaccessible forms fail axe', async () => {
+  const { container } = render(<InaccessibleForm />)
+  // NOTE: I can't think of a situation where you'd want to test that some HTML
+  // actually _does_ have accessibility issues... This is only here for
+  // demonstration purposes.
+  expect(await axe(container)).not.toHaveNoViolations()
+})
+
+test('accessible forms pass axe', async () => {
+  const { container } = render(<AccessibleForm />)
+  const result = await axe(container)
+  expect(result).toHaveNoViolations()
+})
